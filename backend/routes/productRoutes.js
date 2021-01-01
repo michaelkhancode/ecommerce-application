9381efc062bf260f05c82e8770ea54673a389a0f
Michael Yohannes <myemail>
2021-01-01 12:14:01 -0500

    created routes folder, and filled it with product routes

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,32 @@
+import express from "express";
+import Product from "../models/productModel.js";
+import asyncHandler from "express-async-handler";
+const router = express.Router();
+
+// description: fetch all products
+// route GET -> /api/products
+// access anybody
+router.get(
+  "/",
+  asyncHandler(async (req, res) => {
+    const products = await Product.find({});
+    res.json(products);
+  })
+);
+
+// description: fetch single product by id
+// route GET -> /api/products/:id
+// access anybody
+router.get(
+  "/:id",
+  asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id);
+    if (product) {
+      res.json(product);
+    } else {
+      res.status(404).json({ message: "product not found" });
+    }
+  })
+);
+
+export default router;
