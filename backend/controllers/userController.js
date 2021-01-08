55c2badf809acb18d509ea412c31dc8142a9f7cf
Michael Yohannes <myemail>
2021-01-07 21:04:36 -0500

    added authorization middleware to protect routes on back end

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -9,6 +9,7 @@ export const authUser = asyncHandler(async (req, res) => {
   if (user && user.matchPassword(password)) {
     res.json({
       _id: user._id,
+      name: user.name,
       email: user.email,
       isAdmin: user.isAdmin,
       token: generateToken(user._id),
@@ -17,4 +18,20 @@ export const authUser = asyncHandler(async (req, res) => {
     res.status(401);
     throw new Error("invalid email or password");
   }
+});
+
+export const getUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error("user not found");
+  }
+  res.send("Success");
 });
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,16 @@
 import express from "express";
-import { authUser } from "../controllers/userController.js";
+import { authUser, getUserProfile } from "../controllers/userController.js";
+import { protect } from "../middleware/authMiddleware.js";
 const router = express.Router();
 
 // description: Auth the user and get a token
 // route POST -> /api/users/login
 // access public
 router.post("/login", authUser);
+
+// description: Get user profile info
+// route GET -> /api/users/profile
+// access private
+router.route("/profile").get(protect, getUserProfile);
 
 export default router;
