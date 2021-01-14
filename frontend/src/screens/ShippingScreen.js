5b380b12967450c417ed8ebf855a006baafd9c1f
Michael Yohannes <myemail>
2021-01-14 11:32:37 -0500

    shippin screen and save shipping action created

---
+++ frontend/src/screens/ShippingScreen.js
@@ -0,0 +1,71 @@
+import React, { useState, useEffect } from "react";
+import { Link, useHistory } from "react-router-dom";
+import { Form, Button } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import FormContainer from "../components/FormContainer";
+import { saveShippingAdress } from "../actions/cartActions";
+
+export default function ShippingScreen() {
+  const history = useHistory();
+  const [address, setAddress] = useState();
+  const [city, setCity] = useState();
+  const [postal, setPostal] = useState();
+  const [country, setCountry] = useState();
+  const dispatch = useDispatch();
+  const submitHandler = (e) => {
+    e.preventDefault();
+    dispatch(saveShippingAdress({ address, city, postal, country }));
+    history.push("/payment");
+  };
+  return (
+    <FormContainer>
+      <h1>Shipping</h1>
+      <Form onSubmit={submitHandler}>
+        <Form.Group controlId="address">
+          <Form.Label>Address</Form.Label>
+          <Form.Control
+            required
+            type="text"
+            placeholder="Enter address"
+            value={address}
+            onChange={(e) => setAddress(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+
+        <Form.Group controlId="city">
+          <Form.Label>city</Form.Label>
+          <Form.Control
+            required
+            type="text"
+            placeholder="Enter city"
+            value={city}
+            onChange={(e) => setCity(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+
+        <Form.Group controlId="postal">
+          <Form.Label>postal Code</Form.Label>
+          <Form.Control
+            required
+            type="text"
+            placeholder="Enter postal code"
+            value={postal}
+            onChange={(e) => setPostal(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+
+        <Form.Group controlId="country">
+          <Form.Label>country</Form.Label>
+          <Form.Control
+            required
+            type="text"
+            placeholder="Enter country"
+            value={country}
+            onChange={(e) => setCountry(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+        <Button type="submit">Proceed to Payment</Button>
+      </Form>
+    </FormContainer>
+  );
+}
